6698e78a57833a4759efadf8f48394baf7c79174
Rustam Ismailov <ri@cashup.com.ua>
2018-12-18 23:13:50 +0400

    Added build.gradle file, fixed Kafka listener class package

--- src/main/java/com/gauravg/config/KafkaConfig.java
+++ src/main/java/com/gauravg/config/KafkaConfig.java
@@ -19,7 +19,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.kafka.listener.config.ContainerProperties;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
